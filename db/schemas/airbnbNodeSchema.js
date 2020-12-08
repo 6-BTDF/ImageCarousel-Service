5977b6e98ad88dbd655852ddd0c85b4470252169
Harry Clemente <hclemente1989@gmail.com>
2020-12-08 15:19:15 -0800

    Completed schema and seeding scripts

--- db/schemas/airbnbNodeSchema.js
+++ db/schemas/airbnbNodeSchema.js
@@ -1,3 +1,16 @@
+// arangoimport --file "./db/data/userData.csv" --type csv --collection "users" --server.database "airbnb"
+
+
+// arangoimport --file "./db/data/listingData.csv" --type csv --collection "listings" --server.database "airbnb"
+
+// arangoimport --file "./db/data/listData.csv" --type csv --collection "favorite_lists" --server.database "airbnb"
+
+// arangoimport --file "./db/data/listingDetailsData.csv" --type csv --collection "listings" --server.database "airbnb"
+
+// arangoimport --file "./db/data/listListingEdgeData.csv" --type csv --collection "edges" --server.database "airbnb"
+
+// arangoimport --file "./db/data/userListEdgeData.csv" --type csv --collection "edges" --server.database "airbnb"
+
 var Database = require('arangojs').Database;
 var db = new Database();
 const username = 'root';
@@ -7,66 +20,172 @@ db.dropDatabase('airbnb');
 
 function cb() {
   db.useDatabase('airbnb');
-  var listingSchema = {
-    rule : {
-      type : 'object',
-      properties : {
-        listingName : {
-          type : 'string',
-        },
-        listingDescription : {
-          type : 'string',
-        },
-        listingLocation : {
-          type : 'string',
-        },
-        listingStars : {
-          type : 'number',
-        },
-        listingNumReviews : {
-          type : 'number',
-        },
-        photos : {
-          type : 'array',
-        },
-      },
-    },
-  }
   var userSchema = {
-    rule :
-      type        : 'object'
-      properties  : {
-        userName    : {
-          type    : 'string'
+    "message": "Failed schema validation for users",
+    "level": "moderate",
+    "rule": {
+      "properties": {
+        "first_name": {
+          "type": "string"
         },
-        listings  : {
-          type    : 'array'
-        },
-        favorites : {
-          type    : 'array',
-        },ls
+        "last_name": {
+          "type": "string"
+        },"email": {
+          "type": "string"
         }
-  }
-
+      },
+      "required": [
+        "user_id",
+        "first_nume",
+        "last_name",
+        "email"
+      ]
+    }
+  };
+  var favoriteListSchema = {
+    "message": "Failed schema validation for listings",
+    "level": "moderate",
+    "rule": {
+      "list": {
+        "list_id": {
+          "type": "number"
+        },
+        "user_id": {
+          "type": "string"
+        },
+        "listing_name": {
+          "type": "string"
+        },
+        "fav_pic": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "listing_id",
+        "user_id",
+        "listing_name",
+        "fav_pic"
+      ]
+    }
+  };
+  var listingDetailsSchema = {
+    "message": "Failed schema validation for listing details",
+    "level": "moderate",
+    "rule": {
+      "properties": {
+        "property_id": {
+          "type": "string"
+        },
+        "description": {
+          "type": "string"
+        },
+        "listing_name": {
+          "type": "string"
+        },
+        "listing_stars": {
+          "type": "string"
+        },
+        "listing_num_reviews": {
+          "type": "number"
+        },
+        "listing_location": {
+          "type": "string"
+        },
+        "guests": {
+          "type": "number"
+        },
+        "bedrooms": {
+          "type": "number"
+        },
+        "baths": {
+          "type": "number"
+        },
+        "weekday_rate": {
+          "type": "number"
+        },
+        "weekend_event_rate": {
+          "type": "number"
+        },
+        "photos": {
+          "type": "array"
+        },
+        "fav_pic": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "property_id",
+        "description",
+        "listing_name",
+        "listing_location"
+      ]
+    }
+  };
+  var edgeSchema = {
+    rule: {
+      properties: {
+        _to: {type: "string"},
+        _from: {type: "string"},
+      },
+      required: ["_to", "_from"]
+    },
+    level: "moderate",
+    message: "Failed schema validation for edges"
+};
+  var propertyCollection = db.collection('properties');
+  var edgeCollection = db.collection('edges');
   var userCollection = db.collection('users');
   var listingCollection = db.collection('listings');
-  listingCollection.create({ 'listingSchema': listingSchema})
-  .then(
-    () => console.log('listings created'),
-    err => console.error('Failed to create listings:', err)
+  propertyCollection.create().then(
+    () => console.log('Collection created'),
+    err => console.error('Failed to create collection:', err)
   )
   .then(() => {
-    userCollection.create({ 'userSchema': userSchema})
-    .then(
-      () => console.log('users created'),
-      err => console.error('Failed to create users:', err)
-    )
-    .catch((err) => console.log(err));
+    userCollection.create().then(
+    () => console.log('Collection created'),
+    err => console.error('Failed to create collection:', err)
+  )
   })
+  .then(() => {
+    listingCollection.create().then(
+      () => console.log('Collection created'),
+      err => console.error('Failed to create collection:', err)
+    )
+  })
+  .then(() => {
+    edgeCollection.create().then(
+      () => console.log('Collection created'),
+      err => console.error('Failed to create collection:', err)
+    )
+  })
+  .catch((err) => console.log(err));
 
-
-
-
+  // listingCollection.create({ 'listingSchema': listingSchema})
+  // .then(
+  //   () => console.log('listing collection created')
+  // )
+  // .then(() => {
+  //   userCollection.create({ 'userSchema': userSchema})
+  //   .then(
+  //     () => console.log('user collection created')
+  //     )
+  //     .catch((err) => console.log(err))
+  //   .then(() => {
+  //     edgeCollection.create({'edgeSchema': edgeSchema})
+  //     .then(
+  //       () => console.log('edge collection created')
+  //     )
+  //     .catch((err) => console.log(err))
+  //   .then( () => {
+  //     propertyCollection.create({'propertySchema': propertySchema})
+  //     .then(
+  //       () => console.log('property collection created')
+  //     )
+  //     .catch((err) => console.log(err))
+  //   })
+  //   })
+  //   .catch((err) => console.log(err))
+  // })
 }
 
 db.createDatabase('airbnb')
--- db/schemas/airbnbPostgresSchema.sql
+++ db/schemas/airbnbPostgresSchema.sql
@@ -2,29 +2,32 @@ DROP SCHEMA airbnb CASCADE;
 CREATE SCHEMA airbnb;
 
 CREATE TABLE airbnb.users (
-  userId SERIAL PRIMARY KEY,
-  userName VARCHAR(100) NOT NULL
+  user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
 );
 
-CREATE TABLE airbnb.listings (
-  listingName VARCHAR(100) NOT NULL,
-  userId INT REFERENCES users(userId),
+CREATE TABLE airbnb.favorite_list (
+  listing_id SERIAL PRIMARY KEY,
+  listing_name VARCHAR(100) REFERENCES listing_details(listing_name),
+  user_id INT REFERENCES users(user_id),
+  fav_pic : VARCHAR(100) REFERENCES listing_details(fav_pic),
 );
 
---JOIN
-CREATE TABLE airbnb.favoriteListings (
-  favoriteId SERIAL PRIMARY KEY,
-  listingId INT REFERENCES listingDetails(listingId),
-  userId INT REFERENCES users(userId),
-  order SMALLINT NOT NULL
-  -- order 1 is favorite pic
-)
-
-CREATE TABLE airbnb.listingDetails (
-  listingId SERIAL PRIMARY KEY,
-  listingName VARCHAR(100) NOT NULL,
-  listingDescription VARCHAR(250) NOT NULL,
-  listingLocation VARCHAR(100) NOT NULL,
-  listingNumReviews SMALLINT,
-  photos VARCHAR[]
+CREATE TABLE airbnb.listing_details (
+  listing_details_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(user_id),
+  listing_name VARCHAR(100) NOT NULL,
+  listing_description VARCHAR(250) NOT NULL,
+  listing_location VARCHAR(100) NOT NULL,
+  listing_num_reviews SMALLINT,
+  listing_stars DECIMAL,
+  guests SMALLINT,
+  bedrooms SMALLINT,
+  baths DECIMAL,
+  weekday_rate SMALLINT,
+  weekend_event_rate SMALLINT,
+  photos VARCHAR[],
+  fav_pic : VARCHAR(100) NOT NULL,
 );
